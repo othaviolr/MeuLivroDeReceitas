24a984c1f82f9f7c33957e23d729d1f7e327c4ce
othaviolr <othavionogueira2003@gmail.com>
2025-03-31 19:13:50 -0300

    feat: adiciona endpoint de login e regras de negócio

--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection.Metadata.Ecma335;
 using Moq;
+using MyRecipeBook.Domain.Entities;
 using MyRecipeBook.Domain.Repositories.User;
 
 namespace CommonTestUtilities.Repositories;
@@ -13,6 +14,10 @@ public class UserReadOnlyRepositoryBuilder
     public void ExistActiveUserWithEmail(string email)
     {
         _repository.Setup(repository => repository.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+    public void GetByEmailAndPassword(User user)
+    {
+        _repository.Setup(repository => repository.GetByEmailAndPassword(user.Email, user.Password)).ReturnsAsync(user);
     }
 
     public IUserReadOnlyRepository Build() => _repository.Object;
